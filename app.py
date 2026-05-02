00cd03857825515713be08bead03cc613c37c7c1
Varsha010101 <vg4871@srmist.edu.in>
2026-05-02 12:25:17 +0530

    Fixed green

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+VERSION = os.getenv("APP_VERSION", "unknown")
+
 @app.route('/')
 def home():
-    return "BLUE VERSION"
+    return f"{VERSION.upper()} VERSION"
+
+@app.route('/health')
+def health():
+    return "OK", 200
+
+@app.route('/ready')
+def ready():
+    return "READY", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
